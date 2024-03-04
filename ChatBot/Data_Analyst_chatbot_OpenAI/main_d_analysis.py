bdd4f7bbfee082c5ccc73ac80310605b4e3c33f7
Utkarsh Tripathi <66895751+tripathishivam1444@users.noreply.github.com>
2024-03-04 11:05:04 +0530

    Update main_d_analysis.py

--- ChatBot/Data_Analyst_chatbot_OpenAI/main_d_analysis.py
+++ ChatBot/Data_Analyst_chatbot_OpenAI/main_d_analysis.py
@@ -9,6 +9,7 @@ st.header("u can upload CSV data file hare: ")
 
 #capture the CSV file
 data = st.file_uploader("upload your CSV file", type = "csv")
+st.markdown("<iframe src='https://lottie.host/embed/61073385-a44c-4813-bee2-7bb0bdc62c25/7epTOycPHw.json' width='600' height='400' scrolling='no'></iframe>", unsafe_allow_html=True)
 
 query = st.text_area("Enter Your Query ")
 button = st.button("Genrate Responce ")
